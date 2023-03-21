6b1065b47006f310f55b948ae3817a76b5c5e9f3
Suzzycodes <suzzywrites@gmail.com>
2023-03-21 14:59:43 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @a: an integer
+ * Return: always 0
+ */
+
+int print_last_digit(int a)
+{
+	int k;
+
+	k = a % 10;
+	if (a < 0)
+		k = -k;
+	return (k);
+}
