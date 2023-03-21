96300fd9206145036cf56d5a7d11da144e51a14e
Suzzycodes <suzzywrites@gmail.com>
2023-03-21 15:31:29 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a: para 1
+ * @b: para 2
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	f
