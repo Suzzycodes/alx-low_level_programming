31ffda8542346a12ef8ba2a6b41ac4f7c23ea884
Suzzycodes <suzzywrites@gmail.com>
2023-03-23 10:01:58 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print a long line
+ * @n: number of times to print n
+ * Return: alwsys 0
+ */
+
+void print_line(int n)
+{
+	char _;
+
+	for (n = 0 ; n >= 8 ; n++)
+	{
+		_putchar(_);
+	if (n >= 0)
+	continue;
+	else
+		_putchar('\n');
+	}
+}
