f169efa1e8c51a16d81a374c5552eb7b34141e79
Suzzycodes <suzzywrites@gmail.com>
2023-03-23 08:07:50 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - for printing numbers
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
