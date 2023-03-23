724e33b61d6232f48ae8842c6cdde4eb08e8663c
Suzzycodes <suzzywrites@gmail.com>
2023-03-23 09:28:39 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints more numbers
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i = 1;
+	int j;
+
+	while (i <= 10)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+			_putchar(j + '0');
+		i++;
+		_putchar('\n');
+	}
+}
