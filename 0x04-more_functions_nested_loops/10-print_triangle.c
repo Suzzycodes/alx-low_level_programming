799868a977e59f7b6292edca503d1bb698f6bfca
Suzzycodes <suzzywrites@gmail.com>
2023-03-24 08:49:35 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,21 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
-	if (size <= 0)
+ 	if (size <= 0) 
 		_putchar('\n');
-	for (i = 0 ; i < size ; i++)
+	for (i = 0; i < size; i++) 
 	{
-		for (j = 0 ; j <= i ; j++)
+		for (j = 0; j < size-i-1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
 		{
 			_putchar('#');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
+	_putchar('\n');
 }
