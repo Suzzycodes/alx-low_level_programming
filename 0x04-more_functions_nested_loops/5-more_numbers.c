2c941691947c1b6cd7e76ee6a0c53bac7c12199a
Suzzycodes <suzzywrites@gmail.com>
2023-03-24 04:34:35 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,24 +7,19 @@
 
 void more_numbers(void)
 {
-	int i = 1;
+	int i;
 	int j;
 
-	while (i <= 10)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
 			if (j >= 10) 
 			{
 				_putchar('0'+ j / 10);
-				_putchar('0' + j % 10);
 			}
-			else
-			{
-				_putchar(j + '0');
-			}
+				_putchar('0' + j % 10);	
 		}
-	i++;
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
