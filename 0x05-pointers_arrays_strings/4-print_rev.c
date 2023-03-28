5fa9c30e119d998c6a0701808d0362a4e785a784
Suzzycodes <suzzywrites@gmail.com>
2023-03-28 05:13:42 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter to be used
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
