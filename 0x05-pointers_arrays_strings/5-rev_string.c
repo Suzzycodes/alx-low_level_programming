dbb40f5470c05f42c006dad768c3250d91d5a90a
Suzzycodes <suzzywrites@gmail.com>
2023-03-28 06:18:56 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: parameter to be used
+ * Return: nothing here
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		return;
+}
