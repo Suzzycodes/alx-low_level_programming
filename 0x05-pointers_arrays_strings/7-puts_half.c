8a6a6a15ed38291f9a144470d13d2bc1de15acca
Suzzycodes <suzzywrites@gmail.com>
2023-03-28 06:56:15 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: parameter to be used
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else if (length % 2 != 0)
+	{
+		for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
