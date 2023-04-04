aaa66e52f2739c9ea0df1e3a465f245718852e48
Suzzycodes <suzzywrites@gmail.com>
2023-04-04 03:17:26 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: first parameter
+ * @needle: second parameter
+ * Return: empty
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+	char *p = haystack;
+	char *q = needle;
+
+	while (*q != '\0' && *p == *q)
+	{
+		p++;
+		q++;
+	}
+	if (*q == '\0')
+	{
+		return (haystack);
+	}
+	haystack++;
+	}
+	return (NULL);
+}
