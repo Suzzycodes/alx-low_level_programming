7a34daada1dd5c0fba8b3d7b05d637520f42b193
Suzzycodes <suzzywrites@gmail.com>
2023-04-04 04:16:35 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print sum of two diagonals
+ * @a: first parameter
+ * @size: second parameter
+ * Return: always 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+	sum1 += *(a + i * size + i);
+	sum2 += *(a + i * size + (size - i - 1));
+	}
+	printf("Diagonal 1 sum: %d\n", sum1);
+	printf("Diagonal 2 sum: %d\n", sum2);
+}
