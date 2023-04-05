3da3bfa9c4c3a0f585be9a1a77eac2cd5e50d87a
Suzzycodes <suzzywrites@gmail.com>
2023-04-05 09:52:02 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "math.h"
+/**
+ * helperFunction - checks for square root
+ * @num: first parameter
+ * @pSqrt: second parameter
+ * Return: sqrt of number and -1 for error
+ */
+int helperFunction(int num, int pSqrt)
+{
+	if ((pSqrt * pSqrt) == num)
+	{
+		return (pSqrt);
+	}
+	else
+	{
+		if ((pSqrt * pSqrt) > num)
+			return (-1);
+		else
+			return (helperFunction(num, pSqrt + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - return square root
+ * @n: our parameter
+ * Return: squareroot of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	       	return (-1);
+	else
+		return (helperFunction(n, 0));
+}
