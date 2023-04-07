45bca9eb16a32d9c8f55a55ffa8cd477c13f5aab
Suzzycodes <suzzywrites@gmail.com>
2023-04-07 05:20:06 -0700

    task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 1 || argc > 1)
+	if (argc < 1)
 	{
 		printf("Error\n");
 		return (1);
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check if string has digit
+ * @str: parameter here
+ * Return: always 0
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: first argument
+ * @argv: secong argument
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+	if (check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]);
+	sum += str_to_int;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+
+
+
+
