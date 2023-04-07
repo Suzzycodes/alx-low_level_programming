938661fb65419318bab1c49aecc35d7533dc1a3e
Suzzycodes <suzzywrites@gmail.com>
2023-04-07 04:05:16 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - my main fuction
+ * @argc: first argument
+ * @argv: second argument
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
