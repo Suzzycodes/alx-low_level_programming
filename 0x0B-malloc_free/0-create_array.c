d7fe9b5bca6c70a221ec47d3dcce3ad37add86a7
Suzzycodes <suzzywrites@gmail.com>
2023-04-12 14:34:10 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * create_array - lets create an array of chars
+ * @size: first parameter
+ * @c: second parameter
+ * Return:
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	j
