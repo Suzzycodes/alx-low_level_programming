0f5859c0102689e6490067b71f18e761a86f01e5
Suzzycodes <suzzywrites@gmail.com>
2023-04-12 15:55:24 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - return pointer to array of integers
+ * @width: first para
+ * @height: second parameter
+ * Return: always 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **mee;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	mee = malloc(sizeof(int *) * height);
+	if (mee == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		mee[x] = malloc(sizeof(int) * width);
+		if (mee[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mee[x]);
+			free(mee);
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+	return (mee);
+}
