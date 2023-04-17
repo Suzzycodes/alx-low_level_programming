fb137ab872b0277bdf140259ce91d73ceba1a0c6
Suzzycodes <suzzywrites@gmail.com>
2023-04-17 08:37:19 -0700

    task 4

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -12,5 +13,9 @@ int main(void)
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
 	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+
+	free(my_dog->name);
+	free(my_dog->owner);
+	free(my_dog);
 	return (0);
 }
