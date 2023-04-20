27694efd3a975a83fc5e6c3e3568c1dd9075f8c4
Suzzycodes <suzzywrites@gmail.com>
2023-04-20 04:30:24 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 98 (Error), 99 (Error), or 100 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func_ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func_ptr = get_op_func(argv[2]);
+
+	if (func_ptr == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = func_ptr(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
